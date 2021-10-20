3180796c565f0e8f2283eabf10d9990ee384ac20
Betoml5 <betoml5@hotmail.com>
2021-10-20 10:41:52 -0500

    Add punch and hug

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const Anime_Images = require("anime-images-api");
+const API = new Anime_Images();
 const { Client, Intents, MessageEmbed } = require("discord.js");
 const { config } = require("./config");
 const fetch = require("node-fetch");
@@ -23,7 +25,6 @@ client.on("ready", () => {
 const prefix = "!"; // just an example, change to whatever you want
 var apikey = "0AY2GAYCRBBG";
 
-
 const getWeather = async (city) => {
   try {
     const response = await fetch(
@@ -41,6 +42,44 @@ client.on("messageCreate", async (message) => {
 
   const args = message.content.trim().split(/ +/g);
   const cmd = args[0].slice(prefix.length).toLowerCase(); // case INsensitive, without prefix
+
+  if (cmd == "hug") {
+    const { image } = await API.sfw.hug();
+    if (message.mentions.members.first()) {
+      const exampleEmbed = new MessageEmbed()
+        .setColor("RED")
+        .setImage(image)
+        .setTitle(
+          `${message.author.username} le dio un fuerte abrazo a ${
+            message.mentions.users.first().username
+          }`
+        )
+        .setTimestamp()
+        .setFooter("Made by Betoml5");
+      message.channel.send({ embeds: [exampleEmbed] });
+    } else {
+      message.channel.send("Tagea a alguien para abrazar");
+    }
+  }
+
+  if (cmd == "punch") {
+    const { image } = await API.sfw.punch();
+    if (message.mentions.members.first()) {
+      const exampleEmbed = new MessageEmbed()
+        .setColor("RED")
+        .setImage(image)
+        .setTitle(
+          `${message.author.username} golpeo a ${
+            message.mentions.users.first().username
+          }`
+        )
+        .setTimestamp()
+        .setFooter("Made by Betoml5");
+      message.channel.send({ embeds: [exampleEmbed] });
+    } else {
+      message.channel.send("Tagea a alguien para golpear");
+    }
+  }
 
   if (cmd == "clima") {
     if (args[1] != "") {
@@ -104,181 +143,6 @@ client.on("messageCreate", async (message) => {
     }
   }
 
-  if (cmd == "llorar") {
-    const lmt = 56;
-    const search_term = "cry";
-    const search_url =
-      "https://g.tenor.com/v1/search?q=" +
-      search_term +
-      "&key=" +
-      apikey +
-      "&limit=" +
-      lmt;
-
-    const getCryGif = async () => {
-      const res = await fetch(search_url);
-      const gif = await res.json();
-
-      return gif;
-    };
-    const gif = await getCryGif();
-    const randomNumber = Math.floor(Math.random() * gif.results.length);
-
-    if(args[1]){
-        const exampleEmbed = new MessageEmbed()
-      .setColor("RED")
-      .setTitle(
-        `${message.author.username} hizo llorar a ${
-          message.mentions.users.first().username
-        }`
-      )
-      .setDescription(`${message.author.username} es muy malo`)
-      .setImage(gif.results[randomNumber].media[0].gif.url)
-      .setTimestamp()
-      .setFooter("Made by Betoml5");
-
-    message.channel.send({ embeds: [exampleEmbed] });
-    } else{
-        message.channel.send("Necesito a alguien para hacerlo llorar. Intriseco")
-    }
-  }
-
-  if (cmd == "punch") {
-    const lmt = 56;
-    const search_term = "punch";
-    const search_url =
-      "https://g.tenor.com/v1/search?q=" +
-      search_term +
-      "&key=" +
-      apikey +
-      "&limit=" +
-      lmt;
-
-    const getPunchGif = async () => {
-      const res = await fetch(search_url);
-      const gif = await res.json();
-
-      return gif;
-    };
-    const gif = await getPunchGif();
-    const randomNumber = Math.floor(Math.random() * gif.results.length);
-
-    const exampleEmbed = new MessageEmbed()
-      .setColor("RED")
-      .setTitle(
-        `${message.author.username} GOLPEO a ${
-          message.mentions.users.first().username
-        }`
-      )
-      .setDescription(`${message.author.username} es muy malo`)
-      .setImage(gif.results[randomNumber].media[0].gif.url)
-      .setTimestamp()
-      .setFooter("Made by Betoml5");
-
-    message.channel.send({ embeds: [exampleEmbed] });
-  }
-
-  if (cmd == "kiss") {
-    const lmt = 56;
-    const search_term = "kiss";
-    const search_url =
-      "https://g.tenor.com/v1/search?q=" +
-      search_term +
-      "&key=" +
-      apikey +
-      "&limit=" +
-      lmt;
-
-    const getKissGif = async () => {
-      const res = await fetch(search_url);
-      const gif = await res.json();
-
-      return gif;
-    };
-    const gif = await getKissGif();
-    const randomNumber = Math.floor(Math.random() * gif.results.length);
-
-    const exampleEmbed = new MessageEmbed()
-      .setColor("RED")
-      .setTitle(
-        `${message.author.username} beso a ${
-          message.mentions.users.first().username
-        }`
-      )
-      .setDescription(`${message.author.username} es muy amoroso`)
-      .setImage(gif.results[randomNumber].media[0].gif.url)
-      .setTimestamp()
-      .setFooter("Made by Betoml5");
-
-    message.channel.send({ embeds: [exampleEmbed] });
-  }
-
-  if (cmd == "cum") {
-    const lmt = 56;
-    const search_term = "cummed";
-    const search_url =
-      "https://g.tenor.com/v1/search?q=" +
-      search_term +
-      "&key=" +
-      apikey +
-      "&limit=" +
-      lmt;
-
-    const getCumGif = async () => {
-      const res = await fetch(search_url);
-      const gif = await res.json();
-
-      return gif;
-    };
-    const gif = await getCumGif();
-    const randomNumber = Math.floor(Math.random() * gif.results.length);
-
-    const exampleEmbed = new MessageEmbed()
-      .setColor("RED")
-      .setTitle(
-        `${message.author.username} cumeo a ${
-          message.mentions.users.first().username
-        }`
-      )
-      .setDescription(`${message.author.username} es muy amoroso`)
-      .setImage(gif.results[randomNumber].media[0].gif.url)
-      .setTimestamp()
-      .setFooter("Made by Betoml5");
-
-    message.channel.send({ embeds: [exampleEmbed] });
-  }
-
-  if (cmd == "lol") {
-    const lmt = 56;
-    const search_term = "league of legends";
-    const search_url =
-      "https://g.tenor.com/v1/search?q=" +
-      search_term +
-      "&key=" +
-      apikey +
-      "&limit=" +
-      lmt;
-
-    const getCumGif = async () => {
-      const res = await fetch(search_url);
-      const gif = await res.json();
-
-      return gif;
-    };
-    const gif = await getCumGif();
-    const randomNumber = Math.floor(Math.random() * gif.results.length);
-
-    const exampleEmbed = new MessageEmbed()
-      .setColor("RED")
-      .setTitle(`${message.author.username} quiere jugar LOL`)
-      .setDescription(`${message.author.username} se quiere muy poco`)
-      .setImage(gif.results[randomNumber].media[0].gif.url)
-      .setTimestamp()
-      .setFooter("Made by Betoml5");
-
-    message.channel.send({ embeds: [exampleEmbed] });
-  }
-
   if (cmd == "cat") {
     const lmt = 56;
     const search_term = "cat";
@@ -326,7 +190,9 @@ client.on("messageCreate", async (message) => {
       .setColor("RED")
       .setTitle("Presentacion de Ruperto Martinez")
       .setDescription(text)
-      .setImage("https://cdn.shopify.com/s/files/1/0022/9739/7308/files/fd_large.jpg")
+      .setImage(
+        "https://cdn.shopify.com/s/files/1/0022/9739/7308/files/fd_large.jpg"
+      )
       .setTimestamp()
       .setFooter("Made by Betoml5");
 
